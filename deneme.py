b2eb5214257b2c88f86c7c2ffaac191584dcb848
mehmetali13020 <mehmetali13020@gmail.com>
2026-02-02 19:24:03 +0300

    turn system added

--- deneme.py
+++
@@ -1,7 +0,0 @@
-grid = [[0 for i in range(8)]for j in range(8)]
-for i in range(8):
-    for j in range(8):
-        if (i+j) %2 == 0:
-            grid[i][j] = " "
-        else:
-            grid[i][j] = "#"
