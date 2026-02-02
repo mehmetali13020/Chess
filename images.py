1d9ed4187d86a6dcadb3bbd04ccf825ba6bdb480
mehmetali13020 <mehmetali13020@gmail.com>
2026-02-02 14:58:30 +0300

    Add files via upload

---
+++ images.py
@@ -0,0 +1,17 @@
+import pygame
+pygame.init()
+white_pawn_img   = pygame.image.load("images/white-pawn.png")
+white_rook_img   = pygame.image.load("images/white-rook.png")
+white_knight_img = pygame.image.load("images/white-knight.png")
+white_bishop_img = pygame.image.load("images/white-bishop.png")
+white_queen_img  = pygame.image.load("images/white-queen.png")
+white_king_img   = pygame.image.load("images/white-king.png")
+black_pawn_img   = pygame.image.load("images/black-pawn.png")
+black_rook_img   = pygame.image.load("images/black-rook.png")
+black_knight_img = pygame.image.load("images/black-knight.png")
+black_bishop_img = pygame.image.load("images/black-bishop.png")
+black_queen_img  = pygame.image.load("images/black-queen.png")
+black_king_img   = pygame.image.load("images/black-king.png")
+
+#"images/white-pawn.png"
+#/Desktop/Ders/Algo/python/Chess\?/
